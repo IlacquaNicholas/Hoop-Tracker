8c3a467a4ab9671875d3ecc0461117e6de844f93
Nicholas Ilacqua <Niservices88@gmail.com>
2022-01-03 16:29:40 -0600

    Worked on the date displaying

--- src/components/DatePage/DatePage.jsx
+++ src/components/DatePage/DatePage.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
 
-class DatePage extends React.Component {
-    state = {
-        myCurrentTime: new Date().toLocaleString(),
+import "react-datepicker/dist/react-datepicker.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class DatePage extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            startDate: new Date()
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
+
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        })
+    }
+
+    onFormSubmit(e) {
+        e.preventDefault();
+        console.log(this.state.startDate)
+    }
+
     render() {
         return (
-            <div className="App">
-                <p>Current Date and Time: {this.state.myCurrentTime}</p>
-            </div>
+            <form onSubmit={this.onFormSubmit}>
+                <div className="form-group">
+                    <DatePicker
+                        selected={this.state.startDate}
+                        onChange={this.handleChange}
+                        name="startDate"
+                        dateFormat="MM/dd/yyyy"
+                    />
+                    <button className="btn btn-primary">Show Date</button>
+                </div>
+            </form>
         );
     }
+
 }
 
 export default DatePage;
