6d9cf221713e48f16fd3602601e3f3c8d6f53f9a
Nicholas Ilacqua <Niservices88@gmail.com>
2022-01-17 12:38:24 -0600

    added some stuff on the about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import'./AboutPage.css'
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
@@ -9,7 +9,33 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <h1>What is Hoop Tracker?</h1>
+        <p>Hoop tracker is an app to help someone keep stats for a basketball game. 
+          I came up with the idea while playing in a league for the last 7 years. 
+          Our team can never remember what we scored a few games in the past. 
+          I thought that this app could help with that.</p>
+
+          <h1>Technologies used:</h1>
+          <ul>
+            <li>React</li>
+            <li>React-Redux</li>
+            <li>Node.js</li>
+            <li>Sagas</li>
+            <li>PostgreSQL</li>
+            <li>Material UI</li>
+            <li>CSS</li>
+          </ul>
+
+
+          <h1>What I would like to add to the project:</h1>
+        <ul>
+          <li>A leader board</li>
+          <li>Find a more efficient way for the stat keeper.</li>
+        </ul>
+
+
+
+
       </div>
     </div>
   );
